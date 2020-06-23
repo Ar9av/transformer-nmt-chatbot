4c4f64f3678ff014835b90d98e51c56bbe707128
Arnav Gupta <f2016731@pilani.bits-pilani.ac.in>
2020-06-23 21:14:49 +0530

    Raise error on incorrect format of training data

--- dataloader.py
+++ dataloader.py
@@ -7,11 +7,14 @@ def loader(config_path):
     with open(os.path.join(config_path, "data/train.to")) as t, open(os.path.join(config_path, "data/train.from")) as f:
         fr = f.readlines()
         to = t.readlines()
-        for i in tqdm.tqdm(range(len(fr))):
-            if "removed" in fr[i] or "deleted" in fr[i] or "removed" in to[i] or "deleted" in to[i] or not fr[i] or not to[i] :
-                continue
-            ds.append(fr[i])
-            ds.append(to[i])
+        try:
+            for i in tqdm.tqdm(range(len(fr))):
+                if "removed" in fr[i] or "deleted" in fr[i] or "removed" in to[i] or "deleted" in to[i] or not fr[i] or not to[i] :
+                    continue
+                ds.append(fr[i])
+                ds.append(to[i])
+        except:
+            raise Exception("Please Recheck, Number of lines in train.to and train.from datafiles should be same.")
     
     f = codecs.open(os.path.join(config_path, "data/training_data.txt"), "w", "utf-8")
     for ele in ds:
